aeec34804961478959a19be844679bb5ec5a3463
bab75 <dealbab75@gmail.com>
2025-05-27 20:38:20 -0400

    Update 4_📊_Charts.py

--- pages/4_📊_Charts.py
+++ pages/4_📊_Charts.py
@@ -65,7 +65,9 @@ with st.sidebar:
         "1 Month": "1mo",
         "3 Months": "3mo",
         "6 Months": "6mo",
-        "1 Year": "1y"
+        "1 Year": "1y",
+        "2 Years": "2y",
+        "5 Years": "5y"
     }
     
     period_display = st.selectbox(
