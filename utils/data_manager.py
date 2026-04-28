4d1f2c51324eee387944d13fb8b89c289982f53a
bab75 <dealbab75@gmail.com>
2026-04-28 18:49:50 -0400

    Restore forward fill for missing values
    Re-enable forward fill for missing values in data.

--- utils/data_manager.py
+++ utils/data_manager.py
@@ -156,9 +156,8 @@ class DataManager:
         data = data.dropna(how='all')
         
         # Forward fill any missing values (common for volume on weekends)
-         #data = data.fillna(method='ffill')
-         data = data.ffill()
-         return data
+         data = data.fillna(method='ffill')
+       
 
 
          
